87fa9699398099e02cfe6489b88b09ad8d6c68ae
Weston Miller <weston.miller@banno.com>
2016-03-02 10:11:38 -0600

    COFED
    This is a bare beginning where I am going through and establishing the
    functions to be overridden. Not all are included as of yet.

---
+++ src/com/riskybusiness/Cofed.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2016  Coved Oswald, Kaleb Luse, and Weston Miller
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.riskybusiness;
+
+import com.riskybusiness.LuxAgentAdapter;
+import com.sillysoft.lux.Board;
+import com.sillysoft.lux.Country;
+
+/**
+ * <p>&nbsp&nbsp&nbsp&nbspThe {@code Cofed} agent uses ANN's to play
+ * a game of RISK.</p>
+ * @author Weston Miller
+ * @author Coved W Oswald
+ * @author Kaleb Luse
+ * @version 1.0
+ * @see com.riskybusiness.LuxAgentAdapter
+ * @since 1.6
+ */
+public class Cofed extends LuxAgentAdapter
+{
+  public Cofed()
+  {
+  }
+
+  @Override
+  public void attackPhase()
+  {
+  }
+
+  @Override
+  public void cardsPhase()
+  {
+  }
+
+  @Override
+  public void fortifyPhase()
+  {
+  }
+
+  @Override
+  public int message()
+  {
+  }
+
+  @Override
+  public int moveArmiesIn()
+  {
+  }
+
+  @Override
+  public String name()
+  {
+    return "WESTIN";
+  }
+
+  @Override
+  public int pickCountry()
+  {
+  }
+
+  @Override
+  public int placeArmies()
+  {
+  }
+}
