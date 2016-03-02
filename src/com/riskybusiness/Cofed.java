ed57677bcf7bd18d556b835009c0500dfa6d3558
Weston Miller <weston.miller@banno.com>
2016-03-02 17:15:14 -0600

    COFED
    I added the constructor and altered the spacing in hopes of making it
    more readable. Also, the method of instantiating the helper agent
    (EvilPixie) is established.

--- src/com/riskybusiness/Cofed.java
+++ src/com/riskybusiness/Cofed.java
@@ -17,10 +17,11 @@
 
 package com.riskybusiness;
 
-import java.util.Random;
 import com.riskybusiness.LuxAgentAdapter;
 import com.sillysoft.lux.Board;
 import com.sillysoft.lux.Country;
+import com.sillysoft.lux.agent.EvilPixie;
+import java.util.Random;
 
 /**
  * <p>&nbsp&nbsp&nbsp&nbspThe {@code Cofed} agent uses ANN's to play
@@ -34,77 +35,87 @@ import com.sillysoft.lux.Country;
  */
 public class Cofed extends LuxAgentAdapter
 {
-  public Cofed()
-  {
-  }
+    public Cofed()
+    {
+    }
 
-  @Override
-  public void attackPhase()
-  {
-    return this.agent.attackPhase();
-  }
+    @Override
+    public void setPrefs(int ID, Board board)
+    {
+        super.setPrefs(ID, board);
+        this.agent = new EvilPixie();
+        this.agent.setPrefs(ID, board);
+    }
 
-  @Override
-  public void cardsPhase(Card[] cards)
-  {
-    return this.agent.cardsPhase(cards);
-  }
+    @Override
+    public void attackPhase()
+    {
+        // This is here as filler. It will be altered to the appropriate
+        // attackPhase according to our own AI
+        return this.agent.attackPhase();
+    }
 
-  @Override
-  public void fortifyPhase()
-  {
-    return this.agent.fortifyPhase();
-  }
+    @Override
+    public void cardsPhase(Card[] cards)
+    {
+        return this.agent.cardsPhase(cards);
+    }
 
-  @Override
-  public int message(String note, Object data)
-  {
-    return this.agent.message(note, data);
-  }
+    @Override
+    public void fortifyPhase()
+    {
+        return this.agent.fortifyPhase();
+    }
 
-  @Override
-  public int moveArmiesIn(int countryCodeAttacker, int countryCodeDefender)
-  {
-    return this.agent.moveArmiesIn(countryCodeAttacker, countryCodeDefender);
-  }
+    @Override
+    public int message(String note, Object data)
+    {
+        return this.agent.message(note, data);
+    }
 
-  @Override
-  public int pickCountry()
-  {
-      return this.agent.pickCountry();
-  }
+    @Override
+    public int moveArmiesIn(int countryCodeAttacker, int countryCodeDefender)
+    {
+        return this.agent.moveArmiesIn(countryCodeAttacker, countryCodeDefender);
+    }
 
-  @Override
-  public void placeArmies()
-  {
-    return this.agent.placeArmies();
-  }
+    @Override
+    public int pickCountry()
+    {
+        return this.agent.pickCountry();
+    }
 
-  @Override
-  public void placeInitialArmies(int numberOfArmies)
-  {
-    return this.agent.placeInitialArmies(numberOfArmies);
-  }
+    @Override
+    public void placeArmies()
+    {
+        return this.agent.placeArmies();
+    }
 
-  @Override
-  public String youWon()
-  {
-    String[] answers = { "The world is now my footstool",
-  		"Sucks to suck, suckers!",
-  		"Lelouch vi Britannia has nothing on me!",
-  		"Can you smell that? That is the smell of sweet victory",
-  		"Your failure was secured from turn one",
-  		"My skills are without equal",
-  		"Next step: galactic conquest",
-  		"You ignorant fools thought you actually stood a chance?",
-  		"Join me, and I will spare you!\n \n Oh wait, you're already dead",
-  		"Even the Doctor can't save you now!",
-  		"Your head shall be added to my trophy room",
-  		"Now, I just need to make sure Goku stays out \n
-      of my way and I will be unstoppable!",
-      "Onward to pierce the heavens!"
-    };
+    @Override
+    public void placeInitialArmies(int numberOfArmies)
+    {
+        return this.agent.placeInitialArmies(numberOfArmies);
+    }
 
-  	return answers[ Random.nextInt(answers.length) ];
-  }
+    @Override
+    public String youWon()
+    {
+        String[] answers = { "The world is now my footstool",
+          "Sucks to suck, suckers!",
+          "Lelouch vi Britannia has nothing on me!",
+  		    "Can you smell that? That is the smell of sweet victory",
+  		    "Your failure was secured from turn one",
+  		    "My skills are without equal",
+  		    "Next step: galactic conquest",
+  		    "You ignorant fools thought you actually stood a chance?",
+  		    "Join me, and I will spare you!\n \n Oh wait, you're already dead",
+  		    "Even the Doctor can't save you now!",
+  		    "Your head shall be added to my trophy room",
+  		    "Now, I just need to make sure Goku stays out \n
+            of my way and I will be unstoppable!",
+          "Onward to pierce the heavens!"
+        };
+
+  	    return answers[ Random.nextInt(answers.length) ];
+    }
 }
