9d40fdcd54aa1fa23bfc0ad80176b84beba13043
Weston Miller <weston.miller@banno.com>
2016-03-29 18:15:30 -0500

    Worked on the attackPhase method
    the attackPhase method fires

--- src/com/riskybusiness/Cofed.java
+++ src/com/riskybusiness/Cofed.java
@@ -37,7 +37,8 @@ import java.util.Random;
  */
 public class Cofed extends LuxAgentAdapter
 {
-    protected NeuralNet nn;
+    private int inputNeurons = 0;
+    private NeuralNet nn;
 
     public Cofed()
     {
@@ -49,51 +50,52 @@ public class Cofed extends LuxAgentAdapter
         super.setPrefs(ID, board);
         this.agent = new EvilPixie();
         this.agent.setPrefs(ID, board);
-	//Setup NeuralNet
+	      //Setup NeuralNet
 
-	/*
-	 * Finds the number of country neurons.
-	 * Each country must have a CountryID,
-	 * a ContinentID, a PlayerID, the number
-	 * of troops in that country, and whether
-	 * or not we can attack it. 6 total inputs
-	 * for each country. In the network, these
-	 * inputs are each their own neuron,
-	 * ordered in the manner presented above.
-	 */
-	       int inputNeurons = this.countries.length * 6;
+	      /*
+	       * Finds the number of country neurons.
+	       * Each country must have a CountryID,
+	       * a ContinentID, a PlayerID, the number
+	       * of troops in that country, and whether
+	       * or not we can attack it. 5 total inputs
+	       * for each country. In the network, these
+	       * inputs are each their own neuron,
+	       * ordered in the manner presented above.
+	       */
+	      inputNeurons = this.countries.length * 5;
 
-	/*
-	 * Finds the number of player neurons.
-	 * These are after the country neurons
-	 * and take the same order that they do.
-	 * Each player needs a PlayerID, the amount
-	 * of cards that player has, and the amount
-	 * of reinforcements that player has per
-	 * turn.
-	 */
-	       inputNeurons += this.board.getNumberOfPlayers() * 3;
+	      /*
+	       * Finds the number of player neurons.
+         * These are after the country neurons
+	       * and take the same order that they do.
+         * Each player needs a PlayerID, the amount
+         * of cards that player has, and the amount
+         * of reinforcements that player has per
+         * turn.
+         */
+	      inputNeurons += this.board.getNumberOfPlayers() * 3;
 
-	/*
-	 * Simple algorithm to calculate how many
-	 * bits it will take to represent the amount
-	 * of countries given to the board. Then add
-	 * one to say attack or not. This will be
-	 * the output layer of the network.
-	 */
-	       int outputNeurons = 1;
+        /*
+         * Simple algorithm to calculate how many
+         * bits it will take to represent the amount
+         * of countries given to the board. Then add
+         * one to say attack or not. This will be
+         * the output layer of the network.
+         */
+	       int outputNeurons = 0;
 	       int countryCounter = this.countries.length;
 	       while(countryCounter > 0)
 	       {
 	           outputNeurons++;
 	           countryCounter = countryCounter >> 1;
 	       }
-
-	//Create a neural network
-	//For now, the network will have 6 layers
-	//in total. All four of the hidden layers
-	//will contain 12 neurons for consistencies
-	//sake.
+         outputNeurons *= 2; //for defending country code;
+         outputNeurons += 1; //for the actual attack or not.
+          //Create a neural network
+          //For now, the network will have 6 layers
+          //in total. All four of the hidden layers
+          //will contain 12 neurons for consistencies
+          //sake.
 	       this.nn = new NeuralNet(inputNeurons, outputNeurons, 12, 12, 12, 12);
     }
 
@@ -133,8 +135,8 @@ public class Cofed extends LuxAgentAdapter
         int ownCards = this.board.getPlayerCards(this.ID);
         int enemyTroops = country.getArmies();
         int enemyCards = this.board.getPlayerCards(country.getOwner());
-        int ownReinforcementsPerTurn = this.getPlayerIncome(this.ID);
-        int enemyReinforcementsPerTurn = this.getPlayerIncome(country.getOwner());
+        int ownReinforcementsPerTurn = this.board.getPlayerIncome(this.ID);
+        int enemyReinforcementsPerTurn = this.board.getPlayerIncome(country.getOwner());
         boolean opponentLastCountry = (BoardHelper.getPlayerCountries(country.getOwner(), this.countries) == 1) ? true : false;
         boolean breakContinentBonus = BoardHelper.anyPlayerOwnsContinent(country.getContinent(), this.countries);
 
@@ -300,202 +302,8 @@ public class Cofed extends LuxAgentAdapter
             }
         }
 
-        return new int[] {id, fitness, attackValue};
+        return new int[] {fitness, attackValue};
     }
-
-    private int defenseHeuristic(Country country)
-    {
-        private int fitness = 0;
-        private int attackValue = 0; // Default to not attack
-        boolean canAttack = false;
-        outer:
-        for(int i : country.getHostileAdjoiningCodeList())
-        {
-            for(Country j : this.countries)
-            {
-                if(j.getOwner() == this.ID && j.getCode() != i && j.canGoto(i))
-                {
-                    canAttack = true;
-                    break outer;
-                }
-            }
-        }
-
-        int ownTroops = BoardHelper.getPlayerArmies(this.ID, this.countries);
-        int ownCards = this.board.getPlayerCards(this.ID);
-        int enemyTroops = country.getArmies();
-        int enemyCards = this.board.getPlayerCards(country.getOwner());
-        int ownReinforcementsPerTurn = this.getPlayerIncome(this.ID);
-        int enemyReinforcementsPerTurn = this.getPlayerIncome(country.getOwner());
-        boolean ownLastCountry = (BoardHelper.getPlayerCountries(this.ID, this.countries) == 1) ? true : false;
-        boolean breakContinentBonus = BoardHelper.anyPlayerOwnsContinent(country.getContinent(), this.countries);
-
-        if (canAttack)
-        {
-            if (PlayerID == 0)
-            {
-
-                if (ownTroops == enemyTroops)
-                {
-                    if (ownTroops <= 5)
-                    {
-                        if (ownLastCountry)
-                        {
-                            if ((ownCards + enemyCards) >= 5)
-                            {
-                                //Don't Attack
-                                fitness += 4;
-                            }
-                            else if (enemyReinforcementsPerTurn <= ownReinforcementsPerTurn)
-                            {
-                                //Don't Attack
-                                fitness += 6;
-                            }
-                            else
-                            {
-                                //Don't Attack
-                                fitness += 5;
-                            }
-                        }
-                        else if (breakContinentBonus)
-                        {
-                            //Don't Attack
-                            fitness += 3;
-                        }
-
-                        else
-                        {
-                            //Don't Attack
-                            fitness += 7;
-                        }
-                    }
-
-                    else if (ownTroops <= 15)
-                    {
-                        attackValue += 1; //Attack
-                        fitness += 5;
-                    }
-
-                    else
-                    {
-                        attackValue += 1; //Attack
-                        fitness += 3;
-                    }
-                }
-
-                else if (ownTroops > enemyTroops)
-                {
-                    if ((ownTroops - enemyTroops) >= 10)
-                    {
-                        attackValue += 1; //Attack
-                        fitness += 10;
-                    }
-
-                    else if ((ownTroops - enemyTroops) >= 6)
-                    {
-                        attackValue += 1; //Attack
-                        fitness += 8;
-                    }
-
-                    else if ((ownTroops - enemyTroops) >= 2)
-                    {
-                        attackValue += 1; //Attack
-                        fitness += 6;
-                    }
-
-                    else
-                    {
-                        if (ownLastCountry)
-                        {
-                            if ((ownCards + enemyCards) >= 5)
-                            {
-                                //Don't Attack
-                                fitness += 4;
-                            }
-                            else if (enemyReinforcementsPerTurn <= ownReinforcementsPerTurn)
-                            {
-                                attackValue += 1; //Attack
-                                fitness += 5;
-                            }
-                            else
-                            {
-                                //Don't Attack
-                                fitness += 5;
-                            }
-                        }
-
-                        else if (breakContinentBonus)
-                        {
-                            //Don't Attack
-                            fitness += 4;
-                        }
-
-                        else
-                        {
-                            //Don't Attack
-                            fitness += 6;
-                        }
-                    }
-                }
-
-                else
-                {
-                    if ((enemyTroops - ownTroops) >= 2)
-                    {
-                        if (enemyTroops >= 20)
-                        {
-                            //Don't Attack
-                            fitness += 3;
-                        }
-
-                        else if (enemyTroops >= 13)
-                        {
-                            if (ownLastCountry)
-                            {
-                                if ((ownCards + enemyCards) >= 5)
-                                {
-                                    //Don't Attack
-                                    fitness += 2;
-                                }
-
-                                else if (enemyReinforcementsPerTurn <= ownReinforcementsPerTurn)
-                                {
-                                    //Don't Attack
-                                    fitness += 5;
-                                }
-
-                                else
-                                {
-                                    attackValue += 1; //Attack
-                                    fitness += 6;
-                                }
-                            }
-
-                            else if (breakContinentBonus)
-                            {
-                                //Don't Attack
-                                fitness += 4;
-                            }
-
-                            else
-                            {
-                                //Don't Attack
-                                fitness += 5;
-                            }
-                        }
-                    }
-
-                    else
-                    {
-                        attackValue += 1; //Attack
-                        fitness += 9;
-                    }
-                }
-            }
-        }
-
-          return new int[] {id, fitness, attackValue};
-      }
 
     @Override
     public void attackPhase()
@@ -503,12 +311,15 @@ public class Cofed extends LuxAgentAdapter
         // declare lists to save the results of the Heuristics
         // stored in form (countryID, fitnessValue, attackValue)
         //ArrayList<Integer[]> attackList = new ArrayList<Integer[]>();
-        ArrayList<Integer[]> attackList = new ArrayList<Integer[]>();
-        ArrayList<Integer[]> defenseList = new ArrayList<Integer[]>();
+        //ArrayList<Integer[]> attackList = new ArrayList<Integer[]>();
+        //ArrayList<Integer[]> defenseList = new ArrayList<Integer[]>();
+        int countryID = 0;
+        int fitness = 0;
+        int attackValue = 0;
         // Gather data from board
 
 
-
+        int[] temp; //for attack;
         // Run & Save Heuristics
         for(Country i : this.countries)
         {
@@ -516,10 +327,15 @@ public class Cofed extends LuxAgentAdapter
             {
                 for(Country j : this.countries)
                 {
-                    if(j.getCode() != i.getCode() && j.canGoto(i))
+                    if(j.getCode() != i.getCode() && j.canGoto(i) && j.getOwner() != this.ID)
                     {
-                        attackList.add(attackHeuristic(j));
-                        defenseList.add(defenseHeuristic(j));
+                        temp = attackHeuristic(j);
+                        if(temp[0] > fitness)
+                        {
+                            countryID = j.getCode();
+                            fitness = temp[0];
+                            attackValue = temp[1];
+                        }
                     }
                 }
             }
@@ -528,7 +344,38 @@ public class Cofed extends LuxAgentAdapter
         // Train NeuralNet
             // TODO
         // Attack or don't attack
-
+        int countryCounter = 0;
+        int playerCounter = 0;
+        float[][] inputs = new int[inputNeurons][1];
+        for(int i = 0; i < inputs.length; i++)
+        {
+            if(i <= this.countries.length)
+            {
+                inputs[i][0] = this.countries[countryCounter].getCode();
+                inputs[++i][0] = this.countries[countryCounter].getContinent();
+                inputs[++i][0] = this.countries[countryCounter].getOwner();
+                inputs[++i][0] = this.countries[countryCounter].getArmies();
+                inputs[++i][0] = (this.countries[countryCounter++].getNumberPlayerNeighbors(this.ID) == 0 || inputs[i - 2] == this.ID) ? 0 : 1;
+            }
+            else
+            {
+                inputs[i][0] = playerCounter; //does it start at zero or 1?
+                inputs[++i][0] = this.board.getPlayerCards(playerCounter);
+                input[++i][0] = this.board.getPlayerIncome(playerCounter++);
+            }
+        }
+        float[] outputs = nn.fire(inputs);
+        char[] countryToAttack = new char[outputs.length / 2];
+        char[] countryToAttackFrom = new char[outputs.length / 2];
+        for(int i = 1; i < outputs.length / 2 + 1; i++)
+            countryToAttack[i - 1] = outputs[i] + '0';
+        for(int i = outputs.length / 2 + 1; i < outputs.length; i++)
+            countryToAttackFrom[i - 1] = outputs[i] + '0';
+        int countryToAttackCode = Integer.parseInt(new String(countryToAttack), 2);
+        int countryToAttackFromCode = Integer.parseInt(new String(countryToAttackFrom), 2);
+        boolean attack = (outputs[0] == 0) ? false : true;
+        if(attack)
+            this.board.attack(countryToAttackCode, countryToAttackFromCode, true);
     }
 
     @Override
@@ -577,16 +424,16 @@ public class Cofed extends LuxAgentAdapter
     public String youWon()
     {
         String[] answers = { "The world is my footstool- and my feet stink",
-          "Sucks to suck, suckers!",
+          "Even the ancient dragons fall before me!",
           "Lelouch vi Britannia has nothing on me!",
   		    "Can you smell that? That is the smell of sweet victory",
   		    "Your failure was secured from turn one",
-  		    "My skills are without equal",
+  		    "My skills are without equal!\n \n Pay no mind to the juggler to my right...",
   		    "Next step: galactic conquest",
   		    "You ignorant fools thought you actually stood a chance?",
   		    "Join me, and I will spare you!\n \n Oh wait, you're already dead",
   		    "Even the Doctor can't save you now!",
-  		    "Next I shall become King of the Pirates!",
+  		    "Next I shall conquer the seas and become King of the Pirates!",
   		    "Now, I just need to make sure Goku stays out \n
             of my way and I will be unstoppable!",
           "I am the drill that will pierce the heavens!"
