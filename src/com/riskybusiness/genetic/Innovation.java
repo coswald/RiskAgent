f759639e78410e0890ef2c7ab6a41a0219818e83
coswald <coswald@users.noreply.github.com>
2016-04-22 10:49:48 -0500

    Finished Commenting Innovation
    Lusey77: Good Code! Don't forget to add @ldm comments to all files you made.

--- src/com/riskybusiness/genetic/Innovation.java
+++ src/com/riskybusiness/genetic/Innovation.java
@@ -18,6 +18,28 @@
 package com.riskybusiness.genetic;
 import java.io.Serializable;
 
+/**
+ * <p>&nbsp&nbsp&nbsp&nbsp&nbspThis class represents a single
+ * {@code Innovation} to be entered into the {@code InnovationDB}.
+ * An {@code Innovation} is any addition to the network that
+ * can be described in a database setting to be used for NEAT.
+ * A single {@code Innovation} consists of an {@code InnovationType},
+ * and different values depending on whether or not this type
+ * is adding link or a neuron. If a neuron is to be added,
+ * then the neuron is put inbetween two neurons on a link,
+ * making the "sending" neuron the left neuron, and the
+ * "receiving" neuron the right neuron, while the neuron's id
+ * is set to the added neurons ID. However, if a link is to be
+ * added, the sending and receiving neurons are what you think
+ * they would be for a link, but the neuron ID is set to -1.</p>
+ * @author Kaleb Luse
+ * @author Coved W Oswald
+ * @author Weston Miller
+ * @ldm 22 April 2016
+ * @version 1.0
+ * @since 1.6
+ * @see com.riskybusiness.genetic.InnovationType
+ */
 public class Innovation implements Serializable
 {
 	//Represents the innovation type
@@ -31,7 +53,19 @@ public class Innovation implements Serializable
 	//private int            innovationNum;
 
 	private static final long serialVersionUID = 7260173936646208532L;
-
+	
+	/**
+	 * <p>&nbsp&nbsp&nbsp&nbsp&nbspCreates an {@code Innovation}
+	 * that uses the given parameters to describe a change in
+	 * the network.</p>
+	 * @param type Describes the type of {@code Innovation}.
+	 * @param from Describes the neuron that sends information.
+	 * @param to Describes the neuron that recieves information.
+	 * @param id The id of the neuron to be added, -1 if no
+	 * 			addition of a neuron is taking
+	 * 			place.
+	 * @see com.riskybusiness.genetic.Innovation
+	 */
 	public Innovation(InnovationType type, int from, int to, int id)
 	{
 		this.type = type;
@@ -47,59 +81,113 @@ public class Innovation implements Serializable
 	{
 		return (this.type == innovation.type && this.fromNeuron == innovation.fromNeuron && this.toNeuron == innovation.toNeuron && this.neuronID == innovation.neuronID);
 	}
-
+	
+	/**
+	 * <p>&nbsp&nbsp&nbsp&nbsp&nbspChecks to see if this
+	 * {@code Innovation} is equal to another neuron addition
+	 * {@code Innovation}.</p>
+	 * @param type The type to compare to this ones.
+	 * @param fromNeuron The fromNeuron to compare to this ones.
+	 * @param toNeuron The toNeuron to compare to this ones.
+	 * @param neuronID The neuronID to comapre to this ones.
+	 * @return Whether or not these two {@code Innovations} are equal.
+	 */
 	public boolean isEqual(InnovationType type, int fromNeuron, int toNeuron, int neuronID)
 	{
 		return (this.type == type && this.fromNeuron == fromNeuron && this.toNeuron == toNeuron && this.neuronID == neuronID);
 	}
-
+	
+	/**
+	 * <p>&nbsp&nbsp&nbsp&nbsp&nbspChecks to see if this
+	 * {@code Innovation} is equal to another link addition
+	 * {@code Innovation}.</p>
+	 * @param type The type to compare to this ones.
+	 * @param fromNeuron The fromNeuron to compare to this ones.
+	 * @param toNeuron The toNeuron to compare to this ones.
+	 * @return Whether or not these two {@code Innovations} are equal.
+	 */
 	public boolean isEqual(InnovationType type, int fromNeuron, int toNeuron)
 	{
 		return (this.type == type && this.fromNeuron == fromNeuron && this.toNeuron == toNeuron);
 	}
 
-	//Get whether the innovation is a neuron or a link
+	/**
+	 * <p>&nbsp&nbsp&nbsp&nbsp&nbspReturns the
+	 * {@code InnovationType} of this {@code Innovation}.
+	 * @return The {@code InnovationType} of this class.
+	 * @see com.riskybusiness.genetic.InnovationType
+	 */
 	public InnovationType getType() 
 	{
 		return this.type;
 	}
-
-	//Get synapse input neuronID, return -1 for neurons
+	
+	/**
+	 * <p>&nbsp&nbsp&nbsp&nbsp&nbspReturns the sending
+	 * neuron's identification integer.</p>
+	 * @return The id of the sending neuron.
+	 */
 	public int getFromNeuron() 
 	{
 		return this.fromNeuron;
 	}
 
-	//Get synapse output neuronID, return -1 for neurons
+	/**
+	 * <p>&nbsp&nbsp&nbsp&nbsp&nbspReturns the receiving
+	 * neuron's identification integer.</p>
+	 * @return The id of the receiving neuron.
+	 */
 	public int getToNeuron()
 	{
 		return this.toNeuron;
 	}
 
-	//Get id of the neuron, will return -1 for links
+	/**
+	 * <p>&nbsp&nbsp&nbsp&nbsp&nbspReturns the new
+	 * neuron's identification integer.</p>
+	 * @return The id of the new neuron, -1 if this
+	 * 			is a link {@code Innovation}.
+	 */
 	public int getNeuronID()
 	{
 		return this.neuronID;
 	}
-
+	
+	/**
+	 * <p>&nbsp&nbsp&nbsp&nbsp&nbspChanges the type
+	 * of {@code Innovation} this is.</p>
+	 * @param type The new, different {@code InnovationType}.
+	 */
 	public void setType(InnovationType type) 
 	{
 		this.type = type;
 	}
 
-	//Get synapse input neuronID, return -1 for neurons
+	/**
+	 * <p>&nbsp&nbsp&nbsp&nbsp&nbspChanges the sending
+	 * neuron's id.</p>
+	 * @param neuron The new neuron ID.
+	 */
 	public void setFromNeuron(int neuron) 
 	{
 		this.fromNeuron = neuron;
 	}
 
-	//Get synapse output neuronID, return -1 for neurons
+	/**
+	 * <p>&nbsp&nbsp&nbsp&nbsp&nbspChanges the receiving
+	 * neuron's id.</p>
+	 * @param neuron The new neuron ID.
+	 */
 	public void setToNeuron(int neuron)
 	{
 		this.toNeuron = neuron;
 	}
 
-	//Get id of the neuron, will return -1 for links
+	/**
+	 * <p>&nbsp&nbsp&nbsp&nbsp&nbspChanges the neuron's
+	 * id.</p>
+	 * @param id The new neuron ID.
+	 */
 	public void setNeuronID(int id)
 	{
 		this.neuronID = id;
